72ce7f22afe6d199d623594b08a4a424505bdbf7
Betrezen88 <wyszyn2000@gmail.com>
2018-03-21 15:00:18 +0100

    Added new tabs with enhancement, thresholded and filtration.

--- DiceRecognizer/MainWindow.cpp
+++ DiceRecognizer/MainWindow.cpp
@@ -1,6 +1,7 @@
 #include "MainWindow.h"
 
 #include "CVImageWidget.h"
+#include "ContrastEnhancement.h"
 
 #include <opencv2/opencv.hpp>
 
@@ -26,9 +27,14 @@ void MainWindow::openImage()
     QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), "");
 
     cv::Mat cvImage = cv::imread(fileName.toStdString());
-    cv::Mat cvGray;
 
-    cv::cvtColor(cvImage, cvGray, CV_BGR2GRAY);
+    // Poniższy proces przenieść do osobnej klasy rozdzielonej na konkretne kroki
+    cv::Mat cvGray;
+    cv::Mat cvEnhanced;
+    cv::Mat cvThresholded;
+    cv::Mat cvFiltrated;
+
+    cv::cvtColor(cvImage, cvGray, CV_BGR2GRAY); // Convert input image to gray
 
     CVImageWidget* inputImage = new CVImageWidget(this);
     inputImage->showImage(cvImage);
@@ -36,9 +42,28 @@ void MainWindow::openImage()
     CVImageWidget* grayImage = new CVImageWidget(this);
     grayImage->showImage(cvGray);
 
+    cvEnhanced = ContrastEnhancement(cvGray, 60.0/50.0).image(); // Enhance gray image
+
+    CVImageWidget* enhancedImage = new CVImageWidget(this);
+    enhancedImage->showImage(cvEnhanced);
+
+    cv::threshold(cvGray, cvThresholded, 172, 255, CV_THRESH_BINARY);
+
+    CVImageWidget* thresholdedImage = new CVImageWidget(this);
+    thresholdedImage->showImage(cvThresholded);
+
+    cv::dilate(cvThresholded, cvFiltrated, cv::Mat(), cv::Point(-1, -1), 2);
+    cv::erode(cvFiltrated, cvFiltrated, cv::Mat(), cv::Point(-1, -1), 1);
+
+    CVImageWidget* filtratedImage = new CVImageWidget(this);
+    filtratedImage->showImage(cvFiltrated);
+
     m_tabWidget = new QTabWidget(this);
     m_tabWidget->addTab(inputImage, tr("Input Image"));
     m_tabWidget->addTab(grayImage, tr("Gray Image"));
+    m_tabWidget->addTab(enhancedImage, tr("Enhanced Image"));
+    m_tabWidget->addTab(thresholdedImage, tr("Enhanced Image"));
+    m_tabWidget->addTab(filtratedImage, tr("Filtrated Image"));
 
     setCentralWidget(m_tabWidget);
 }
